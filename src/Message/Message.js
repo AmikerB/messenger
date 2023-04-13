e0dbb0c734e254c3a0f0957b6f2596d8a95b81af
AmikerB <118816092+AmikerB@users.noreply.github.com>
2023-04-13 10:50:48 +0100

    simple messenger set up

--- src/Message/Message.js
+++ src/Message/Message.js
@@ -2,38 +2,16 @@ import React, { useEffect, useState } from "react";
 import { db } from "../firebase";
 import { addDoc, collection, onSnapshot } from "firebase/firestore";
 
-// doc, getDoc, updateDoc
-
 const Message = () => {
 
-    // const [countId, setCounterId] = useState(null);
     const [savedMessages, setSavedMessages] = useState([]);
     const [newMessage, setNewMessage] = useState('');
-
-    // const handleClick = () => {
-    //   if (!countId) {
-    //     const coll = collection(db, 'example');
-    //     addDoc(coll, { count: 1 }).then((docRef) => {
-    //       setCounterId(docRef.id)
-    //     })
-    //   } else {
-    //     const counterDoc = doc(db, 'example', countId);
-    //     getDoc(counterDoc).then((docRef) => {
-    //       const { count } = docRef.data()
-    //       updateDoc(counterDoc, { count: count + 1 })
-    //     })
-
-    //   }
-    // }
-
 
     const handleSubmit = (event) => {
         event.preventDefault()
         const coll = collection(db, 'messages');
         addDoc(coll, { text: newMessage })
     }
-
-
 
     useEffect(() => {
         const coll = collection(db, 'messages');
@@ -64,3 +42,26 @@ const Message = () => {
 }
 
 export default Message;
+
+
+
+
+// COUNTER
+
+  // const [countId, setCounterId] = useState(null);
+  
+    // const handleClick = () => {
+    //   if (!countId) {
+    //     const coll = collection(db, 'example');
+    //     addDoc(coll, { count: 1 }).then((docRef) => {
+    //       setCounterId(docRef.id)
+    //     })
+    //   } else {
+    //     const counterDoc = doc(db, 'example', countId);
+    //     getDoc(counterDoc).then((docRef) => {
+    //       const { count } = docRef.data()
+    //       updateDoc(counterDoc, { count: count + 1 })
+    //     })
+
+    //   }
+    // }
