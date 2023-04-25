f4510c09ff9590087a63ca4d7bf7e52f5151badf
AmikerB <118816092+AmikerB@users.noreply.github.com>
2023-04-25 15:14:19 +0100

    pictures added to chats and owner chat bubble

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,15 +2,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faImage, faPaperclip } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 
+
 const Input = () => {
     return (
         <div className="input">
             <input type='text' placeholder='Type something..' />
             <div className="send">
-                <FontAwesomeIcon icon={faPaperclip} />
-                <Input type="file" style={{ display: 'none' }} id='file' />
+                <FontAwesomeIcon icon={faPaperclip} style={{ fontSize: '22px', color: 'gray', cursor: 'pointer' }} />
+                <input type="file" style={{ display: 'none' }} id='file' />
                 <label htmlFor="file">
-                    <FontAwesomeIcon icon={faImage} />
+                    <FontAwesomeIcon icon={faImage} style={{ fontSize: '22px', color: 'grey', cursor: 'pointer' }} />
                 </label>
                 <button>Send</button>
             </div>
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Message = () => {
     return (
-        <div>Message</div>
+        <div className="message owner">
+            <div className="messageInfo">
+                <img src='https://media.licdn.com/dms/image/D5635AQFaVsUxpyTrpQ/profile-framedphoto-shrink_200_200/0/1680169813161?e=1683036000&v=beta&t=hxe-aaC-7FOUq9CQB48SJG07rHWLV_FyoY11EnmnkBs' />
+                <span>just now</span>
+            </div>
+            <div className="messageContent">
+                <p>hello</p>
+                <img src='https://media.licdn.com/dms/image/C4D03AQEngGDsmDg48Q/profile-displayphoto-shrink_100_100/0/1610457684761?e=1687996800&v=beta&t=6OrsZcPeCWh7zWNShMSJqflA2L3dnE4OU2V4Dh5tz9s' />
+            </div>
+        </div>
     )
 }
 
